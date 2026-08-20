7a31128d797049aa2b3769c1e5729a4d0b933cf9
emilopezcano <emilopezcano@gmail.com>
2026-08-20 18:33:54 +0200

    add log messages

--- R/simulations/Scenario1A/l1std_montecarlo_200_100_1A.R
+++ R/simulations/Scenario1A/l1std_montecarlo_200_100_1A.R
@@ -28,99 +28,79 @@ potencia_l1std_montecarlo_200_100 <- numeric(length(deltas))
 ## Object for saving simulation result (out of control signal)
 senal_l1std_montecarlo_200_100 <- vector("list", length(deltas))
 
-for(i in seq_along(deltas)) 
-{
+for (i in seq_along(deltas)) {
   delta <- deltas[i]
-  
+  start <- Sys.time()
+  cat("Running simulation for delta =", delta, "\n")
+
   senal_delta <- vector("list", mc_chart)
-  
+
   f1 <- func.sim.set(
-    t          = tt,
-    var.teor   = var.teor,
+    t = tt,
+    var.teor = var.teor,
     trend.teor = mu0 + delta,
-    corr.teor  = corr.teor,
-    rho        = rho
+    corr.teor = corr.teor,
+    rho = rho
   )
-  
-  for(g in seq_len(mc_chart)) 
-  {
-    
-    ##########################################################################
+
+  for (g in seq_len(mc_chart)) {
     # 1. UCL Estimation
-    ##########################################################################
-    
     estadistico_h0 <- numeric(mc_reps)
-    
+
     calibrado_h0 <- t(f0(n1))
-    
-    for(b in seq_len(mc_reps))
-    {
-      
+
+    for (b in seq_len(mc_reps)) {
       matriz_grupo_mc <- t(f0(n2))
-      
+
       estadistico_h0[b] <-
         fdahotelling:::stat_L1_std(
           x = calibrado_h0,
           y = matriz_grupo_mc
         )
     }
-    
-    ##########################################################################
     # UCL
-    ##########################################################################
-    
+
     UCL <- quantile(
       estadistico_h0,
       probs = 1 - alpha
     )
-    
-    ##########################################################################
     # 2. Monitoring Phase II
-    ##########################################################################
-    
+
     senal_grafico_l1std_montecarlo_200_100 <- logical(K)
-    
-    for(k in seq_len(K))
-    {
-      
-      if(delta == 0){
-        
+
+    for (k in seq_len(K)) {
+      if (delta == 0) {
         matriz_grupo <- t(f0(n2))
-        
-      }else{
-        
+      } else {
         matriz_grupo <- t(f1(n2))
-        
       }
-      
+
       l1std <-
         fdahotelling:::stat_L1_std(
           x = calibrado_h0,
           y = matriz_grupo
         )
-      
+
       senal_grafico_l1std_montecarlo_200_100[k] <-
         l1std > UCL
     }
-    
+
     senal_delta[[g]] <-
       senal_grafico_l1std_montecarlo_200_100
   }
-  
-  ################################################################################
+
   # Resultado final para delta
-  ################################################################################
-  
+
   senal_l1std_montecarlo_200_100[[i]] <-
     unlist(senal_delta)
-  
+
   potencia_l1std_montecarlo_200_100[i] <-
     mean(senal_l1std_montecarlo_200_100[[i]])
+
+  cat("\t", format(Sys.time() - start, digits = 3), "\n")
 }
 
-################################################################################
 # Save
-################################################################################
 
 save(
   potencia_l1std_montecarlo_200_100,
