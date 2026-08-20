15bf8bc1a9a3fdf75fb7e707287f32ea54154df0
emilopezcano <emilopezcano@gmail.com>
2026-08-20 20:25:39 +0200

    refactor permutations 1A pending

--- R/simulations/Scenario1A/l2std_permutations_200_100_1A.R
+++ R/simulations/Scenario1A/l2std_permutations_200_100_1A.R
@@ -1,231 +1,188 @@
-ncores <- parallel::detectCores() - 1
-cl <- makeCluster(ncores)
-registerDoSNOW(cl)
+# Simulation code for Scenario 1A:
+#   * Standardized L^2 statistic
+#   * Permutations method
+#   * n_1 = 200 (calibration sample size)
+#   * n_2 = 100 (monitoring sample size)
 
+## Theoretical correlation structure (Scenarios A)
+corr.teor <- corr.teor.A
 
-deltas <- c(0,0.3,0.5,0.7,0.9)
-potencia_l2std_perms_150_100 <- numeric(length(deltas))
-senal_l2std_perms_150_100 <- vector("list", length(deltas))
-alpha<-0.05
+## Object for saving simulation result (power)
+potencia_l2std_perms_200_100 <- numeric(length(deltas))
 
+## Object for saving simulation result (out of control signal)
+senal_l2std_perms_200_100 <- vector("list", length(deltas))
+
+## Initialize seeds for reproducibility
 seeds_perm <- 1000 + seq_along(deltas)
-mc <- 1000
-P  <- 500
-K  <- 20
-n1<-150
-n2<-100
 
+## Calibration sample size
+n1 <- 200
 
+## Monitoring sample size
+n2 <- 100
 
-tt <- seq(0, 1, length.out = 25)
-mu0 <- 30 * tt * (1 - tt)^(3/2)
-var.teor <- 1
-corr.teor <- outer(
-  tt,
-  tt,
-  function(s, t) exp(-2 * (s - t)^2)
-)
-
-rho <- 0
-
-
-for(i in seq_along(deltas)) 
-{
-  
+for (i in seq_along(deltas)) {
   delta <- deltas[i]
-  
-  
+  start <- Sys.time()
+  cat("Running simulation for delta =", delta, "\n")
+
   senal <- foreach(
     g = seq_len(mc),
-    .packages = c("fda","fda.usc","fdahotelling"),
-    .export = c(
-      "fdqcd",
-      "fdqcs.depth",
-      "fdqcs.depth.default",
-      "fdqcs.depth.fdqcd",
-      "func.sim.set",
-      "mu0"
-    ),
+    .packages = c("fda", "fda.usc", "fdahotelling", "qcr"),
     .options.RNG = seeds_perm[i]
-  ) %dorng% {
-    
-    
-    f0 <- func.sim.set(
-      t          = tt,
-      var.teor   = var.teor,
-      trend.teor = mu0,
-      corr.teor  = corr.teor,
-      rho        = rho
-    )
-    
-    f1 <- func.sim.set(
-      t          = tt,
-      var.teor   = var.teor,
-      trend.teor = mu0 + delta,
-      corr.teor  = corr.teor,
-      rho        = rho
-    )
-    
-    eps <- 0.05                      
-    
-    n_out <- max(1, ceiling(eps * n1))
-    
-    calibrado <- rbind(
-      t(f0(n1 - n_out)),
-      t(f1(n_out))
-    )
-    
-    
-    calibrado <- calibrado[
-      sample(seq_len(nrow(calibrado))),
-      ,
-      drop = FALSE
-    ]
-    
-    if(delta != 0){
-      
-      fdchart <- fdqcd(as.data.frame(calibrado))
-      
-      fddep <- fdqcs.depth(
-        fdchart,
-        depth = fda.usc::depth.mode,
-        nb = 2000,
-        plot = FALSE,
-        ns = 0.05
+  ) %dorng%
+    {
+      f0 <- func.sim.set(
+        t = tt,
+        var.teor = var.teor,
+        trend.teor = mu0,
+        corr.teor = corr.teor,
+        rho = rho
       )
-      
-      if(length(fddep$out) > 0){
-        
-        calibrado <- calibrado[-fddep$out, , drop = FALSE]
-        
-      }
-      
-    }
-    
-    
-    n_cal <- nrow(calibrado)   
-    
-    n1_perm <- n_cal          
-    n2_perm <- n2             
-    n_total <- n1_perm + n2_perm   
-    
-    perm_estadistico <- numeric(P)
-    
-    
-    
-    ind <- sample(
-      seq_len(n_cal),
-      size = n_total,
-      replace = TRUE
-    )
-    
-    data_perm <- calibrado[ind, , drop = FALSE]
-    
-    
-    ##########################################################################
-    # Permutations
-    ##########################################################################
-    
-    for(p in seq_len(P)){
-      
-      perm <- sample(n_total)
-      
-      
-      D1 <- data_perm[
-        perm[1:n1_perm],
+
+      f1 <- func.sim.set(
+        t = tt,
+        var.teor = var.teor,
+        trend.teor = mu0 + delta,
+        corr.teor = corr.teor,
+        rho = rho
+      )
+
+      eps <- 0.05
+
+      n_out <- max(1, ceiling(eps * n1))
+
+      calibrado <- rbind(
+        t(f0(n1 - n_out)),
+        t(f1(n_out))
+      )
+
+      calibrado <- calibrado[
+        sample(seq_len(nrow(calibrado))),
         ,
         drop = FALSE
       ]
-      
-      D2 <- data_perm[
-        perm[(n1_perm + 1):n_total],
-        ,
-        drop = FALSE
-      ]
-      
-      perm_estadistico[p] <-
-        fdahotelling:::stat_L2_std(
-          x = D1,
-          y = D2
+
+      if (delta != 0) {
+        fdchart <- fdqcd(as.data.frame(calibrado))
+
+        fddep <- fdqcs.depth(
+          fdchart,
+          depth = fda.usc::depth.mode,
+          nb = 2000,
+          plot = FALSE,
+          ns = 0.05
         )
-    }
-    
-    UCL <- quantile(
-      perm_estadistico,
-      probs = 1 - alpha,
-      na.rm = TRUE
-    )
-    
-    
-    ######################################################################
-    # 5. Monitoreo
-    ######################################################################
-    
-    estad_monitor <- numeric(K)
-    
-    
-    for(k in seq_len(K)){
-      
-      if(delta == 0){
-        
-        f0 <- func.sim.set(
-          t          = tt,
-          var.teor   = var.teor,
-          trend.teor = mu0,
-          corr.teor  = corr.teor,
-          rho        = rho
-        )
-        
-        matriz_grupo <- t(f0(n2))
-        
-      }else{
-        
-        f1 <- func.sim.set(
-          t          = tt,
-          var.teor   = var.teor,
-          trend.teor = mu0 + delta,
-          corr.teor  = corr.teor,
-          rho        = rho
-        )
-        
-        matriz_grupo <- t(f1(n2))
-        
+
+        if (length(fddep$out) > 0) {
+          calibrado <- calibrado[-fddep$out, , drop = FALSE]
+        }
       }
-      
-      estad_monitor[k] <-
-        fdahotelling:::stat_L2_std(
-          x = calibrado,
-          y = matriz_grupo
-        )
-      
-      
-    }  
-    
-    senal_grafico <- estad_monitor > UCL
-    
-    list(
-      s = senal_grafico,
-      estad_monitor = estad_monitor,
-      UCL = UCL,
-      per =  perm_estadistico
-    )
-    
-    
-  } 
-  
-  senal_l2std_perms_150_100[[i]]<- unlist(lapply(senal, function(x) x$s))
-  
-  
-  potencia_l2std_perms_150_100[i] <- mean(senal_l2std_perms_150_100[[i]])
+
+      n_cal <- nrow(calibrado)
+
+      n1_perm <- n_cal
+      n2_perm <- n2
+      n_total <- n1_perm + n2_perm
+
+      perm_estadistico <- numeric(P)
+
+      ind <- sample(
+        seq_len(n_cal),
+        size = n_total,
+        replace = TRUE
+      )
+
+      data_perm <- calibrado[ind, , drop = FALSE]
+
+      ##########################################################################
+      # Permutations
+      ##########################################################################
+
+      for (p in seq_len(P)) {
+        perm <- sample(n_total)
+
+        D1 <- data_perm[
+          perm[1:n1_perm],
+          ,
+          drop = FALSE
+        ]
+
+        D2 <- data_perm[
+          perm[(n1_perm + 1):n_total],
+          ,
+          drop = FALSE
+        ]
+
+        perm_estadistico[p] <-
+          fdahotelling:::stat_L2_std(
+            x = D1,
+            y = D2
+          )
+      }
+
+      UCL <- quantile(
+        perm_estadistico,
+        probs = 1 - alpha,
+        na.rm = TRUE
+      )
+
+      ######################################################################
+      # 5. Monitoreo
+      ######################################################################
+
+      estad_monitor <- numeric(K)
+
+      for (k in seq_len(K)) {
+        if (delta == 0) {
+          f0 <- func.sim.set(
+            t = tt,
+            var.teor = var.teor,
+            trend.teor = mu0,
+            corr.teor = corr.teor,
+            rho = rho
+          )
+
+          matriz_grupo <- t(f0(n2))
+        } else {
+          f1 <- func.sim.set(
+            t = tt,
+            var.teor = var.teor,
+            trend.teor = mu0 + delta,
+            corr.teor = corr.teor,
+            rho = rho
+          )
+
+          matriz_grupo <- t(f1(n2))
+        }
+
+        estad_monitor[k] <-
+          fdahotelling:::stat_L2_std(
+            x = calibrado,
+            y = matriz_grupo
+          )
+      }
+
+      senal_grafico <- estad_monitor > UCL
+
+      list(
+        s = senal_grafico,
+        estad_monitor = estad_monitor,
+        UCL = UCL,
+        per = perm_estadistico
+      )
+    }
+
+  senal_l2std_perms_200_100[[i]] <- unlist(lapply(senal, function(x) x$s))
+
+  potencia_l2std_perms_200_100[i] <- mean(senal_l2std_perms_200_100[[i]])
+
+  cat("\t", format(Sys.time() - start, digits = 3), "\n")
 }
 
-
 save(
-  potencia_l2std_perms_150_100,
-  senal_l2std_perms_150_100,
-  file = "l2std_perms_150_100_1A.RData"
+  potencia_l2std_perms_200_100,
+  senal_l2std_perms_200_100,
+  file = "results/simulations/l2std_perms_200_100_1A.RData"
 )
-
-stopCluster(cl)
-
-
