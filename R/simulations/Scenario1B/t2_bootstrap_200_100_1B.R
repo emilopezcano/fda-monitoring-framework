f1f938af0d7d9ac155a8b2357e11edc49c3161a5
emilopezcano <emilopezcano@gmail.com>
2026-08-21 19:27:14 +0200

    Scenario 1B refactored

--- R/simulations/Scenario1B/t2_bootstrap_200_100_1B.R
+++ R/simulations/Scenario1B/t2_bootstrap_200_100_1B.R
@@ -1,292 +1,219 @@
-ncores <- parallel::detectCores() - 1
+# Simulation code for Scenario 1B:
+#   * Hotelling T^2 statistic
+#   * Bootstrap method
+#   * n_1 = 200 (calibration sample size)
+#   * n_2 = 100 (monitoring sample size)
 
-cl <- makeCluster(ncores)
-registerDoSNOW(cl)
+## Theoretical correlation structure (Scenarios B)
+corr.teor <- corr.teor.B
 
-###############################################################################
-# Bootstrap simulation
-################################################################################
-deltas <- c(0,0.3,0.5,0.7,0.9)
+## Object for saving simulation result (out of control signal)
 senal_t2_boot_200_100 <- vector("list", length(deltas))
-smo<-0.05
+
+## Object for saving simulation result (power)
 potencia_t2_boot_200_100 <- numeric(length(deltas))
-alpha<-0.05
-seeds <- 123 + seq_along(deltas)
 
-mc <- 1000
-B  <- 500
-K  <- 20
-tol <- 1e-6
-n1 <-200
-n2<-100
+## Initialize seeds for reproducibility
+seeds <- 345 + seq_along(deltas)
 
+## Calibration sample size
+n1 <- 200
 
-#Mean function
+## Monitoring sample size
+n2 <- 100
 
-tt <- seq(0, 1, length.out = 25)
-mu0 <- 30 * tt * (1 - tt)^(3/2)
+cat("--- Scenario 1B simulation for T2, Bootstrap,", n1, n2, "\n")
 
-#Covariance structure
-
-var.teor <- 1
-
-corr.teor <- outer(
-  tt,
-  tt,
-  function(s, t)
-    exp(-2 * (s - t)^2) * (s + 0.5) * (t + 0.5)
-)
-
-rho <- 0
-
-
-for(i in seq_along(deltas)) 
-{
-  
+for (i in seq_along(deltas)) {
   delta <- deltas[i]
-  
-  ##########################################################################
-  #  Control charts
-  ##########################################################################
-  
-  
+  start <- Sys.time()
+  cat(
+    "[",
+    format(start, "%HH:%MM"),
+    "] Running simulation for delta = ",
+    delta,
+    "\n",
+    sep = ""
+  )
+
   senal <- foreach(
     g = seq_len(mc),
-    .packages = c("fda","fda.usc","fdahotelling"),
-    .export = c(
-      "fdqcd",
-      "fdqcs.depth",
-      "fdqcs.depth.default",
-      "fdqcs.depth.fdqcd",
-      "func.sim.set",
-      "mu0"
-    ),
+    .packages = c("fda", "fda.usc", "fdahotelling", "qcr"),
     .options.RNG = seeds[i]
-  ) %dorng%{
-    
-    ##########################################################################
-    # Generators
-    ##########################################################################
-    
-    f0 <- func.sim.set(
-      t          = tt,
-      var.teor   = var.teor,
-      trend.teor = mu0,
-      corr.teor  = corr.teor,
-      rho        = rho
-    )
-    
-    f1 <- func.sim.set(
-      t          = tt,
-      var.teor   = var.teor,
-      trend.teor = mu0 + delta,
-      corr.teor  = corr.teor,
-      rho        = rho
-    )
-    
-    eps <- 0.05                      
-    
-    n_out <- max(1, ceiling(eps * n1))
-    
-    calibrado <- rbind(
-      t(f0(n1 - n_out)),
-      t(f1(n_out))
-    )
-    
-    #aleatorizar el orden
-    calibrado <- calibrado[
-      sample(seq_len(nrow(calibrado))),
-      ,
-      drop = FALSE
-    ]
-    
-    
-    if(delta != 0){
-      
-      #Recorte
-      fdchart <- fdqcd(as.data.frame(calibrado))
-      
-      fddep <- fdqcs.depth(
-        fdchart,
-        depth = depth.mode,
-        nb = 2000,
-        plot = FALSE,
-        ns = 0.05
-      )
-      
-      if(length(fddep$out) > 0){
-        
-        calibrado <- calibrado[-fddep$out, , drop = FALSE]
-        
-      }
-    }
-    
-    ##########################################################################
-    # Bootstrap parameters
-    ##########################################################################
-    
-    n1_boot <- nrow(calibrado)   
-    n2_boot <- n2                
-    n_boot  <- n1_boot + n2_boot
-    
-    boot_estadistico <- rep(NA_real_, B)
-    
-    mu <- rep(0, ncol(calibrado))
-    
-    
-    
-    for(b in seq_len(B))
+  ) %dorng%
     {
-      
-      ######################################################################
-      # Bootstrap 
-      ######################################################################
-      
-      ind <- sample(
-        seq_len(n1_boot),
-        size = n_boot,
-        replace = TRUE
+      f0 <- func.sim.set(
+        t = tt,
+        var.teor = var.teor,
+        trend.teor = mu0,
+        corr.teor = corr.teor,
+        rho = rho
       )
-      
-      data_boot <- calibrado[ind, , drop = FALSE]
-      
-      
-      cov.est <- var(data_boot)
-      
-      eig <- eigen(cov.est, symmetric = TRUE)
-      
-      lambda <- eig$values
-      V <- eig$vectors
-      
-      if(!all(lambda >= -tol * abs(lambda[1])))
-        next
-      
-      ######################################################################
-      # Smoothed bootstrap
-      ######################################################################
-      
-      L.boot <- V %*%
-        diag(sqrt(pmax(lambda * smo, 0)))
-      
-      dat <- data_boot +
-        t(
-          L.boot %*%
-            matrix(
-              rnorm(n_boot * ncol(data_boot)),
-              nrow = ncol(data_boot)
-            )
-        )
-      
-      
-      boot_1 <- dat[
-        1:n1_boot,
+
+      f1 <- func.sim.set(
+        t = tt,
+        var.teor = var.teor,
+        trend.teor = mu0 + delta,
+        corr.teor = corr.teor,
+        rho = rho
+      )
+
+      eps <- 0.05
+
+      n_out <- max(1, ceiling(eps * n1))
+
+      calibrado <- rbind(
+        t(f0(n1 - n_out)),
+        t(f1(n_out))
+      )
+
+      calibrado <- calibrado[
+        sample(seq_len(nrow(calibrado))),
         ,
         drop = FALSE
       ]
-      
-      boot_2 <- dat[
-        (n1_boot + 1):n_boot,
-        ,
-        drop = FALSE
-      ]
-      
-      ######################################################################
-      #  Hotelling statistic
-      ######################################################################
-      
-      boot_estadistico[b] <-
-        fdahotelling:::stat_hotelling_impl(
-          x = boot_1,
-          y = boot_2,
-          mu = mu,
-          paired = FALSE,
-          step_size = 0.02,
-          use_correction = FALSE,
-          tolerance = tol
+
+      if (delta != 0) {
+        fdchart <- fdqcd(as.data.frame(calibrado))
+
+        fddep <- fdqcs.depth(
+          fdchart,
+          depth = depth.mode,
+          nb = 2000,
+          plot = FALSE,
+          ns = 0.05
         )
-      
-    }
-    
-    ##########################################################################
-    # Control Limit
-    ##########################################################################
-    
-    UCL <- quantile(
-      boot_estadistico,
-      probs = 1 - alpha,
-      na.rm = TRUE
-    )
-    ######################################################################
-    # Phase II Monitoring
-    ######################################################################
-    
-    estad_monitor <- numeric(K)
-    
-    
-    for(k in seq_len(K)){
-      
-      if(delta == 0){
-        
-        f0 <- func.sim.set(
-          t          = tt,
-          var.teor   = var.teor,
-          trend.teor = mu0,
-          corr.teor  = corr.teor,
-          rho        = rho
-        )
-        
-        matriz_grupo <- t(f0(n2_boot))
-        
-      }else{
-        
-        f1 <- func.sim.set(
-          t          = tt,
-          var.teor   = var.teor,
-          trend.teor = mu0 + delta,
-          corr.teor  = corr.teor,
-          rho        = rho
-        )
-        
-        matriz_grupo <- t(f1(n2_boot))
-        
+
+        if (length(fddep$out) > 0) {
+          calibrado <- calibrado[-fddep$out, , drop = FALSE]
+        }
       }
-      
-      estad_monitor[k] <-
-        fdahotelling:::stat_hotelling_impl(
-          x = calibrado,
-          y = matriz_grupo,
-          mu = mu,
-          paired = FALSE,
-          step_size = 0.02,
-          use_correction = FALSE,
-          tolerance = tol
+
+      n1_boot <- nrow(calibrado)
+      n2_boot <- n2
+      n_boot <- n1_boot + n2_boot
+
+      boot_estadistico <- rep(NA_real_, B)
+
+      mu <- rep(0, ncol(calibrado))
+
+      for (b in seq_len(B)) {
+        ind <- sample(
+          seq_len(n1_boot),
+          size = n_boot,
+          replace = TRUE
         )
-      
-      
-    }  
-    
-    senal_grafico <- estad_monitor > UCL
-    
-    list(
-      s = senal_grafico,
-      estad_monitor = estad_monitor,
-      UCL = UCL,
-      boot = boot_estadistico
-    )
-    
-    
-  } 
-  
-  senal_t2_boot_200_100[[i]]  <- unlist(lapply(senal, function(x) x$s))
-  potencia_t2_boot_200_100[i] <-  mean(senal_t2_boot_200_100[[i]])
+
+        data_boot <- calibrado[ind, , drop = FALSE]
+
+        cov.est <- var(data_boot)
+
+        eig <- eigen(cov.est, symmetric = TRUE)
+
+        lambda <- eig$values
+        V <- eig$vectors
+
+        if (!all(lambda >= -tol * abs(lambda[1]))) {
+          next
+        }
+
+        L.boot <- V %*%
+          diag(sqrt(pmax(lambda * smo, 0)))
+
+        dat <- data_boot +
+          t(
+            L.boot %*%
+              matrix(
+                rnorm(n_boot * ncol(data_boot)),
+                nrow = ncol(data_boot)
+              )
+          )
+
+        boot_1 <- dat[
+          1:n1_boot,
+          ,
+          drop = FALSE
+        ]
+
+        boot_2 <- dat[
+          (n1_boot + 1):n_boot,
+          ,
+          drop = FALSE
+        ]
+
+        boot_estadistico[b] <-
+          fdahotelling:::stat_hotelling_impl(
+            x = boot_1,
+            y = boot_2,
+            mu = mu,
+            paired = FALSE,
+            step_size = 0.02,
+            use_correction = FALSE,
+            tolerance = tol
+          )
+      }
+
+      UCL <- quantile(
+        boot_estadistico,
+        probs = 1 - alpha,
+        na.rm = TRUE
+      )
+
+      estad_monitor <- numeric(K)
+
+      for (k in seq_len(K)) {
+        if (delta == 0) {
+          f0 <- func.sim.set(
+            t = tt,
+            var.teor = var.teor,
+            trend.teor = mu0,
+            corr.teor = corr.teor,
+            rho = rho
+          )
+
+          matriz_grupo <- t(f0(n2_boot))
+        } else {
+          f1 <- func.sim.set(
+            t = tt,
+            var.teor = var.teor,
+            trend.teor = mu0 + delta,
+            corr.teor = corr.teor,
+            rho = rho
+          )
+
+          matriz_grupo <- t(f1(n2_boot))
+        }
+
+        estad_monitor[k] <-
+          fdahotelling:::stat_hotelling_impl(
+            x = calibrado,
+            y = matriz_grupo,
+            mu = mu,
+            paired = FALSE,
+            step_size = 0.02,
+            use_correction = FALSE,
+            tolerance = tol
+          )
+      }
+
+      senal_grafico <- estad_monitor > UCL
+
+      list(
+        s = senal_grafico,
+        estad_monitor = estad_monitor,
+        UCL = UCL,
+        boot = boot_estadistico
+      )
+    }
+
+  senal_t2_boot_200_100[[i]] <- unlist(lapply(senal, function(x) x$s))
+  potencia_t2_boot_200_100[i] <- mean(senal_t2_boot_200_100[[i]])
+
+  cat("\t", format(Sys.time() - start, digits = 3), "\n")
 }
-
-
-stopCluster(cl)
 
 save(
   potencia_t2_boot_200_100,
   senal_t2_boot_200_100,
-  file = "t2_boot_200_100_1B.RData"
+  file = "results/simulations/t2_boot_200_100_1B.RData"
 )
-
