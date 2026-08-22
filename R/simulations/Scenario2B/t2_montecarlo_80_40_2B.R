de5a3765e06f6d97b8ac310ee41543c102af2bc1
emilopezcano <emilopezcano@gmail.com>
2026-08-22 22:24:00 +0200

    scenarios 2B refactored

--- R/simulations/Scenario2B/t2_montecarlo_80_40_2B.R
+++ R/simulations/Scenario2B/t2_montecarlo_80_40_2B.R
@@ -1,108 +1,25 @@
-################################################################################
-# Global configuration
-################################################################################
+# Simulation code for Scenario 2B:
+#   * Standardized T2 statistic
+#   * Monte Carlo method
+#   * n_1 = 80 (calibration sample size)
+#   * n_2 = 40 (monitoring sample size)
 
-message(paste(rep("-", 80), collapse = ""))
-message("\tGlobal configuration")
+## Theoretical correlation structure (Scenarios A)
+corr.teor <- corr.teor.B
 
-options(
-  scipen = 99,
-  stringsAsFactors = FALSE
-)
+## Values for etas according to the statistic used (L1, L2, T2)
+etas <- etas.T
 
+## Set seed for reproducibility
 set.seed(123)
 
-################################################################################
-# Simulation parameters
-################################################################################
+## Calibration sample size
+n1 <- 80
 
-mc_chart <- 500  
-mc_reps<-1000
-n1 <- 80   
-n2 <- 40    
-K <- 20   
-tol <- 1e-6
-alpha <- 0.05
-rho <- 0
+## Monitoring sample size
+n2 <- 40
 
-################################################################################
-# 2. Simulation scenarios: 2B
-################################################################################
-
-tt <- seq(0, 1, length.out = 25)
-mu0 <- 30 * tt * (1 - tt)^(3/2)
-var.teor <- 1
-corr.teor <- outer(
-  tt,
-  tt,
-  function(s, t) exp(-2 * (s - t)^2) * (s + 0.5) * (t + 0.5)
-)
-
-
-
-################################################################################
-# Functional data generators
-################################################################################
-
-message("\tCreating functional data generators")
-
-func.sim.set <- function(
-    t,
-    var.teor = 1,
-    trend.teor,
-    corr.teor,
-    rho = 0
-){
-  
-  mdata <- length(t)
-  
-  sd.teor <- sqrt(as.numeric(var.teor))
-  
-  if(rho != 0)
-    corr.teor <- corr.teor *
-    sqrt((1 + rho)/(1 - rho))
-  
-  C <- svd(t(corr.teor))
-  
-  L.corr.teor <- C$u %*% diag(sqrt(C$d))
-  
-  func.sim <- function(rep){
-    
-    err.norm <- matrix(
-      rnorm(mdata * rep),
-      nrow = mdata
-    )
-    
-    data.err <- L.corr.teor %*% err.norm
-    
-    if(rho != 0){
-      
-      data.err[,1] <-
-        data.err[,1] *
-        sqrt((1-rho)/(1+rho))
-      
-      for(i in 2:rep){
-        
-        data.err[,i] <-
-          rho * data.err[,i-1] +
-          (1-rho) * data.err[,i]
-        
-      }
-      
-    }
-    
-    res <- as.numeric(trend.teor) + data.err
-    
-    return(res)
-    
-  }
-  
-  return(func.sim)
-  
-}
-
-
-
+## Phase I function generator
 f0 <- func.sim.set(
   tt,
   var.teor,
@@ -111,41 +28,46 @@ f0 <- func.sim.set(
   rho
 )
 
-################################################################################
-# Monte Carlo T2
-################################################################################
-
-#etas <- c(0, 0.3, 0.5, 0.7, 0.9)
-etas <- c(0, 0.02, 0.03, 0.05)
-
+## Object for saving simulation result (power)
 potencia_t2_montecarlo_80_40 <- numeric(length(etas))
+
+## Object for saving simulation result (out of control signal)
 senal_t2_montecarlo_80_40 <- vector("list", length(etas))
 
-for(i in seq_along(etas)) 
-{
+cat("--- Scenario 2B simulation for T2, Montecarlo,", n1, n2, "\n")
+
+for (i in seq_along(etas)) {
+  start <- Sys.time()
+  cat(
+    "[",
+    format(start, "%HH:%MM"),
+    "] Running simulation for eta = ",
+    eta,
+    "\n",
+    sep = ""
+  )
   eta <- etas[i]
   senal_eta <- vector("list", mc_chart)
   f1 <- func.sim.set(
-    t          = tt,
-    var.teor   = var.teor,
-    trend.teor = (1-eta)*30 * tt * (1 - tt)^(3/2)+eta*30*tt^(3/2)*(1-tt),
-    corr.teor  = corr.teor,
-    rho        = rho
+    t = tt,
+    var.teor = var.teor,
+    trend.teor = (1 - eta) *
+      30 *
+      tt *
+      (1 - tt)^(3 / 2) +
+      eta * 30 * tt^(3 / 2) * (1 - tt),
+    corr.teor = corr.teor,
+    rho = rho
   )
-  
-  for(g in seq_len(mc_chart)) 
-  {
-    
-    
+
+  for (g in seq_len(mc_chart)) {
     estadistico_h0 <- numeric(mc_reps)
     calibrado_h0 <- t(f0(n1))
-    mu<-rep(0,ncol(calibrado_h0))
-    
-    for(b in seq_len(mc_reps))
-    {
-      
+    mu <- rep(0, ncol(calibrado_h0))
+
+    for (b in seq_len(mc_reps)) {
       matriz_grupo_mc <- t(f0(n2))
-      
+
       estadistico_h0[b] <-
         fdahotelling:::stat_hotelling_impl(
           x = as.matrix(calibrado_h0),
@@ -157,26 +79,21 @@ for(i in seq_along(etas))
           tolerance = tol
         )
     }
-    
+
     UCL <- quantile(
       estadistico_h0,
       probs = 1 - alpha
     )
-    
+
     senal_grafico_t2_montecarlo_80_40 <- logical(K)
-    
-    for(k in seq_len(K)){
-      
-      if(eta == 0){
-        
+
+    for (k in seq_len(K)) {
+      if (eta == 0) {
         matriz_grupo <- t(f0(n2))
-        
-      }else{
-        
+      } else {
         matriz_grupo <- t(f1(n2))
-        
       }
-      
+
       T2 <-
         fdahotelling:::stat_hotelling_impl(
           x = calibrado_h0,
@@ -187,24 +104,31 @@ for(i in seq_along(etas))
           use_correction = FALSE,
           tolerance = tol
         )
-      
+
       senal_grafico_t2_montecarlo_80_40[k] <- T2 > UCL
-      
     }
-    
-    
+
     senal_eta[[g]] <- senal_grafico_t2_montecarlo_80_40
-    
   }
-  
+
   senal_t2_montecarlo_80_40[[i]] <- unlist(senal_eta)
-  
+
   potencia_t2_montecarlo_80_40[i] <- mean(senal_t2_montecarlo_80_40[[i]])
+  cat("\t", format(Sys.time() - start, digits = 3), "\n")
 }
 
+
+end <- Sys.time()
+cat(
+    "[",
+    format(end, "%HH:%MM"),
+    "] END simulation Scenario 2B for T2, Montecarlo,", n1, n2, "\n",
+    format(end - start, digits = 3),
+    sep = ""
+  )
 
 save(
   potencia_t2_montecarlo_80_40,
   senal_t2_montecarlo_80_40,
-  file = "t2_montecarlo_80_40_2B.RData"
+  file = "results/simulations/t2_montecarlo_80_40_2B.RData"
 )
